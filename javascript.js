b8ec64d2caabcf0729c866f7485aefc345414f43
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-20 23:54:42 +0530

    use strict

--- javascript.js
+++ javascript.js
@@ -1,2 +1,22 @@
 // console.log can print something on console 
 console.log("hello world");
+
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "Saurabh";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "Shaurya";
+
+console.log(firstName);
