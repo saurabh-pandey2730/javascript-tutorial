22b1c9f8450b6fef0c9f32214417372c76c22172
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-20 23:57:02 +0530

    rules for naming variables

--- javascript.js
+++ javascript.js
@@ -20,3 +20,28 @@ console.log(firstName);
 firstName = "Shaurya";
 
 console.log(firstName);
+
+// rules for naming variables 
+
+// you cannot start with number 
+// example :- 
+// 1value (invalid)
+// value1 (valid)
+
+var value1 = 2;
+console.log(value1);
+
+// you can use only undersore _ or dollar symbol 
+// first_name (valid)
+// _firstname (valid) 
+
+// first$name (valid)
+// $firstname (valid)
+
+// you cannot use spaces 
+// var first_name = "Saurabh"; // snake case writing 
+// var firstName = "Saurabh"; // camel case writing 
+// first name (invalid)
+
+// convention 
+// start with small letter and use camelCase 
