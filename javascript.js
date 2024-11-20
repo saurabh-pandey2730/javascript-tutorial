8b131311e625d5878d3f7a34e67c3523c28c97d8
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 00:00:06 +0530

    String indexing

--- javascript.js
+++ javascript.js
@@ -57,3 +57,20 @@ console.log(firstName);
 
 const pi = 3.14;
 console.log(pi);
+
+// String indexing 
+
+let firstName = "saurabhmanipandey";
+
+//  h    a   r   s   h   i   t 
+//  0    1   2   3   4   5   6
+
+// console.log(firstName[0]);
+// length of string 
+// firstName.length 
+
+console.log(firstName.length);
+
+console.log(firstName[firstName.length-2]);
+
+// last Index : length - 1 
