37776b5d7f87533d0ee72f01e24e40726939e80f
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 00:18:03 +0530

    while loop

--- javascript.js
+++ javascript.js
@@ -418,3 +418,15 @@ switch(day){
     default:
         console.log("Invalid Day");
 }
+// while loop 
+
+// 0 se 9 
+// dry don't repeat yourself
+let i = 0; // 1 2 3 4
+
+while(i<=9){
+    console.log(i);
+    i++;
+}
+console.log(`current value of i is ${i}`);
+console.log("hello");
