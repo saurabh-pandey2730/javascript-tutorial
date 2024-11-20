5a19b2a883e1b49a2c3450b41bf31d9eb9261c9b
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 00:05:13 +0530

    typeof operator

--- javascript.js
+++ javascript.js
@@ -95,3 +95,29 @@ let firstName = "  Saurabh ";
 
 let newString = firstName.slice(1); // Saurabh
 console.log(newString);
+
+// typeof operator 
+
+// data types (primitive data types)
+// string "saurabh"
+// number 2, 4, 5.6 
+// booleans 
+// undefined
+// null 
+// BigInt
+// Symbol
+
+// let age = 22; 
+// let firstName = "harshit";
+
+// // console.log(typeof age);
+
+// // 22 -> "22"
+// // convert number to string. 
+// age = age + "";
+// console.log(typeof(age)); "22"
+
+
+// // convert string to number. 
+
+// let myStr = +"34";
