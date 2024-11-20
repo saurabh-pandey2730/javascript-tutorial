a079290b170fb366442168ab7efd7301468222e6
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 00:14:45 +0530

    and  or operator

--- javascript.js
+++ javascript.js
@@ -253,3 +253,29 @@ console.log(aboutMe);
 // let age = 3;
 // let drink = age >= 5 ? "coffee" : "milk";
 // console.log(drink);
+
+// and  or operator 
+
+
+
+// if(firstName[0] === "H"){
+//     console.log("your name starts with H")
+// }
+
+// if(age > 18){
+//     console.log("you are above 18");
+// }
+
+// if(firstName[0] === "H" && age>18){
+//     console.log("Name starts with H and above 18");
+// }else{
+//     console.log("inside else");
+// }
+let firstName = "arshit";
+let age = 16;
+
+if(firstName[0] === "H" || age>18){
+    console.log("inside if");
+}else{
+    console.log("inside else");
+}
