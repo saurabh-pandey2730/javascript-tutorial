9e63b4a93f62948f1e9019152ace66a70ad79920
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 10:40:28 +0530

    while loop in array

--- javascript.js
+++ javascript.js
@@ -605,3 +605,15 @@ for(let i=0; i < fruits.length; i++){
 }
 
 console.log(fruits2);
+
+
+// while loop in array 
+
+const fruits = ["apple", "mango", "grapes"];
+const fruits2 = [];
+let i = 0;
+while(i<fruits.length){
+    fruits2.push(fruits[i].toUpperCase());
+    i++;
+}
+console.log(fruits2);
