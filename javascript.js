39be4663757a9a9b20870055991745327c48f28d
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 10:35:53 +0530

    for loop

--- javascript.js
+++ javascript.js
@@ -447,3 +447,23 @@ let num = 100;
 
 // let total = (num*(num+1))/2;
 // console.log(total);
+
+// intro to for loop 
+// print 0 to 9
+
+for(let i = 0;i<=9;i++){
+    console.log(i);
+}
+
+// console.log("value of i is ",i);
+// for loop example 
+
+let total = 0;
+
+let num = 100;
+
+for(let i = 1; i<=num; i++){
+    total = total + i;
+}
+
+console.log(total);
