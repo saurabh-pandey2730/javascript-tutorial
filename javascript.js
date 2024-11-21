0ea2f715a0a5700668a47ad43365283911c43b3a
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 10:35:14 +0530

    while loop example

--- javascript.js
+++ javascript.js
@@ -430,3 +430,20 @@ while(i<=9){
 }
 console.log(`current value of i is ${i}`);
 console.log("hello");
+
+// while loop example 
+let num = 100;
+// let total = 0; //1 + 2 +3
+// let i = 0;
+
+
+// while(i<=100){
+//     total = total + i;
+//     i++;
+// }
+
+
+// console.log(total);
+
+// let total = (num*(num+1))/2;
+// console.log(total);
