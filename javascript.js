a0fd63885527aec4898838c4fed625537b6d5ab0
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-21 10:36:14 +0530

    break keywork

--- javascript.js
+++ javascript.js
@@ -467,3 +467,22 @@ for(let i = 1; i<=num; i++){
 }
 
 console.log(total);
+
+// break keywork
+
+// continue keyword 
+
+// for(let i = 1; i<=10; i++){
+//     if(i===4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i<=10; i++){
+//     if(i===4){
+//         continue;
+//     }
+//     console.log(i);
+// }
+console.log("hello there");
