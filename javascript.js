305d0037a7e400c3ea693511b1f3e9c8a58755ed
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-22 11:24:21 +0530

    intro to objects

--- javascript.js
+++ javascript.js
@@ -649,3 +649,29 @@ let [myvar1, myvar2, ...myNewArray] = myArray;
 console.log("value of myvar1", myvar1);
 console.log("value of myvar2", myvar2);
 console.log(myNewArray);
+
+
+// objects reference type  
+// arrays are good but not sufficient 
+// for real world data 
+// objects store key value pairs 
+// objects don't have index
+
+// how to create objects 
+
+// const person = {name:"Harshit",age:22};
+const person = {
+    name: "harshit",
+    age: 22,
+    hobbies: ["guitar", "sleeping", "listening music"]
+}
+console.log(person);
+
+// how to access data from objects 
+// console.log(person["name"]);
+// console.log(person["age"]);
+// console.log(person.hobbies);
+
+// how to add key value pair to objects
+person["person"] = "male";
+console.log(person);
