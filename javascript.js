eed9429641c485d153663c6361766ef1061c1ed0
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-23 11:30:03 +0530

    how to iterate object

--- javascript.js
+++ javascript.js
@@ -689,3 +689,26 @@ console.log(person);
 // console.log(person["person hobbies"]);
 // person[key] = "harshitvashisth@gmail.com";
 // console.log(person);
+
+// how to iterate object 
+const person = {
+    name: "harshit",
+    age: 22,
+    "person hobbies": ["guitar", "sleeping", "listening music"]
+}
+
+// for in loop 
+// Object.keys 
+
+// for(let key in person){
+//     // console.log(`${key} : ${person[key]}`);
+//     console.log(key," : " ,person[key]);
+// }
+
+// console.log(typeof (Object.keys(person)));
+// const val = Array.isArray((Object.keys(person)));
+// console.log(val);
+
+// for(let key of Object.keys(person)){
+//     console.log(person[key]);
+// }
