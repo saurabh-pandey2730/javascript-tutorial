83ef2d86fb101f91a2ef6cb5df6b593704dd6a10
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-23 11:32:26 +0530

    objects inside array

--- javascript.js
+++ javascript.js
@@ -772,3 +772,15 @@ const band = {
   let { bandName, famousSong, ...restProps } = band;
   console.log(bandName);
   console.log(restProps);
+
+  // objects inside array 
+// very useful in real world applications
+
+const users = [
+    {userId: 1,firstName: 'harshit', gender: 'male'},
+    {userId: 2,firstName: 'mohit', gender: 'male'},
+    {userId: 3,firstName: 'nitish', gender: 'male'},
+]
+for(let user of users){
+    console.log(user.firstName);
+}
