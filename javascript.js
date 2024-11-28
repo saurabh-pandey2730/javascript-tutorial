607c9fe126c7664621bac32b158241ccf2493f98
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:37:50 +0530

    nested destructuring

--- javascript.js
+++ javascript.js
@@ -784,3 +784,14 @@ const users = [
 for(let user of users){
     console.log(user.firstName);
 }
+
+// nested destructuring 
+const users = [
+    {userId: 1,firstName: 'harshit', gender: 'male'},
+    {userId: 2,firstName: 'mohit', gender: 'male'},
+    {userId: 3,firstName: 'nitish', gender: 'male'},
+]
+const [{firstName: user1firstName, userId}, , {gender: user3gender}] = users;
+console.log(user1firstName);
+console.log(userId);
+console.log(user3gender);
