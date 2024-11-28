008ba23d4ee017e866e0194e704935946bb8ecd8
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:46:30 +0530

    param destructuring

--- javascript.js
+++ javascript.js
@@ -987,3 +987,22 @@ function addAll(...numbers){
 }
 const ans = addAll(4,5,4,2,10);
 console.log(ans);
+
+// param destructuring 
+// object 
+// react 
+const person = {
+    firstName: "Saurabh",
+    gender: "male",
+    age: 500
+}
+// function printDetails(obj){
+//     console.log(obj.firstName);
+//     console.log(obj.gender);
+// }
+function printDetails({firstName, gender, age}){
+    console.log(firstName);
+    console.log(gender);
+    console.log(age);
+}
+printDetails(person);
