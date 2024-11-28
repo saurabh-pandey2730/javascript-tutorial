3d3aedea8545089aa7879136376a4810880f0e0b
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:49:35 +0530

    important array methods

--- javascript.js
+++ javascript.js
@@ -1027,3 +1027,29 @@ function myFunc(){
 }
 const ans = myFunc();
 console.log(ans());
+
+const numbers = [4,2,5,8];
+// function myFunc(number, index){
+//     console.log(`index is ${index} number is ${number}`);
+// }
+// numbers.forEach(function(number,index){
+//     console.log(`index is ${index} number is ${number}`);
+// });
+// numbers.forEach(function(number, index){
+//     console.log(number*3, index);
+// })
+const users = [
+    {firstName: "Shivam", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+// users.forEach(function(user){
+//     console.log(user.firstName);
+// });
+// users.forEach((user, index)=>{
+//     console.log(user.firstName, index);
+// })
+// for(let user of users){
+//     console.log(user.firstName);
+// }
