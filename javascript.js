2d0f85a5e739c2d1f1ca9822e068f849c8736e3c
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:43:59 +0530

    block scope vs function scope

--- javascript.js
+++ javascript.js
@@ -937,3 +937,23 @@ function myApp(){
     myFunc();
 }
 myApp();
+
+// block scope vs function scope 
+// let and const are block scope
+// var is function scope 
+// if(true){
+//     var firstName = "Saurabh";
+//     console.log(firstName);
+// }
+// console.log(firstName);
+function myApp(){
+    if(true){
+        var firstName = "Saurabh";
+        console.log(firstName);
+    }
+    if(true){
+        console.log(firstName);
+    }
+    console.log(firstName);
+}
+myApp();
