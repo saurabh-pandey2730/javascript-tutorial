ea03139c1df8ccc6814df6db7fdf8f9b597c1a81
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 02:02:40 +0530

    fill method

--- javascript.js
+++ javascript.js
@@ -1283,3 +1283,11 @@ const userCart = [
 ]
 const ans = userCart.some((cartItem)=>cartItem.price > 100000);
 console.log(ans);
+
+// fill method 
+// value , start , end 
+// const myArray = new Array(10).fill(0);
+// console.log(myArray);
+const myArray = [1,2,3,4,5,6,7,8];
+myArray.fill(0,2,5);
+console.log(myArray);
