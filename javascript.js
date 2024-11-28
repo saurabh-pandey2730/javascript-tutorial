fe342099c298e7f4c54490d03005dca548e4e451
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:44:32 +0530

    default parameters

--- javascript.js
+++ javascript.js
@@ -957,3 +957,16 @@ function myApp(){
     console.log(firstName);
 }
 myApp();
+
+// default parameters 
+// function addTwo(a,b){
+//     if(typeof b ==="undefined"){
+//         b = 0;
+//     }
+//     return a+b;
+// }
+function addTwo(a,b=0){
+    return a+b;
+}
+const ans = addTwo(4, 8);
+console.log(ans);
