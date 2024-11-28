336681f21d4d0846de8fbf70704c4019cee155ed
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:50:15 +0530

    map method

--- javascript.js
+++ javascript.js
@@ -1053,3 +1053,23 @@ const users = [
 // for(let user of users){
 //     console.log(user.firstName);
 // }
+
+// map method 
+// const numbers = [3,4,6,1,8];
+// const square = function(number){
+//     return number*number;
+// }
+// const squareNumber = numbers.map((number, index)=>{
+//     return index;
+// });
+// console.log(squareNumber);
+const users = [
+    {firstName: "shivam", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+const userNames = users.map((user)=>{
+    return user.firstName;
+});
+console.log(userNames);
