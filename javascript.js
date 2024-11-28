e91882fd5b22acdefeb313bf3e4ed254b86a539b
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:47:05 +0530

    callback functions

--- javascript.js
+++ javascript.js
@@ -1006,3 +1006,14 @@ function printDetails({firstName, gender, age}){
     console.log(age);
 }
 printDetails(person);
+
+// callback functions 
+function myFunc2(name){
+    console.log("inside my func 2")
+    console.log(`your name is ${name}`);
+}
+function myFunc(callback){
+    console.log("hello there I am a func and I can..")
+    callback("pandey");
+}
+myFunc(myFunc2);
