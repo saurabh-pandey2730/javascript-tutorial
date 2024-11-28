3cce3b3287a805df91ee7007f875c0a67a666fb5
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:50:48 +0530

    filter method

--- javascript.js
+++ javascript.js
@@ -1073,3 +1073,10 @@ const userNames = users.map((user)=>{
     return user.firstName;
 });
 console.log(userNames);
+
+// filter method 
+const numbers = [1,3,2,6,4,8];
+const evenNumbers = numbers.filter((number)=>{
+    return number % 2 === 0;
+});
+console.log(evenNumbers);
