8889a01fc2aad6dba45e85bd1a369ce8acd57dc5
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-29 01:45:13 +0530

    rest parameters

--- javascript.js
+++ javascript.js
@@ -970,3 +970,20 @@ function addTwo(a,b=0){
 }
 const ans = addTwo(4, 8);
 console.log(ans);
+
+// rest parameters 
+// function myFunc(a,b,...c){
+//     console.log(`a is ${a}`);
+//     console.log(`b is ${b}`);
+//     console.log(`c is`, c);
+// }
+// myFunc(3,4,5,6,7,8,9);
+function addAll(...numbers){
+    let total = 0;
+    for(let number of numbers){
+        total = total + number;
+    }
+    return total;
+}
+const ans = addAll(4,5,4,2,10);
+console.log(ans);
