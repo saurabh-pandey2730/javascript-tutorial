8093158239da9976d7e269ee4d78fdb4e6d0d36e
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-30 10:28:17 +0530

    arrow functions

--- javascript.js
+++ javascript.js
@@ -1519,3 +1519,15 @@ const user1 = {
 // user1.about();
 const myFunc = user1.about.bind(user1);
 myFunc();
+
+// arrow functions 
+
+const user1 = {
+    firstName : "pranjal",
+    age: 8,
+    about: () => {
+        console.log(this.firstName, this.age);
+    }   
+}
+
+user1.about(user1);
