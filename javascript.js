c74b4f2cd6190ca7100c62192b89c746224245ae
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-30 10:37:49 +0530

    Object.create(obj1)

--- javascript.js
+++ javascript.js
@@ -1631,3 +1631,30 @@ obj2.key3 = "value3";
 console.log(obj2);
 
 console.log(obj2.__proto__);
+
+const userMethods = {
+    about : function(){
+        return `${this.firstName} is ${this.age} years old.`;
+    },
+    is18 : function(){
+        return this.age >= 18;
+    },
+    sing: function(){
+        return 'toon na na na la la ';
+    }
+}
+function createUser(firstName, lastName, email, age, address){
+    const user = Object.create(userMethods);// {}
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    return user;
+}
+
+
+const user3 = createUser('mohit', 'ved', 'mohit@gmail.com', 17, "my address");
+console.log(user3);
+console.log(user3.about());
+// console.log(user3.sing());
