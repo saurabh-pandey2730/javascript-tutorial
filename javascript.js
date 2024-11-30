ca5ad5d288bbfdd5fe7ef195a841fff2c61cd7a0
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-30 10:44:14 +0530

    Array.prototype

--- javascript.js
+++ javascript.js
@@ -1786,3 +1786,14 @@ for(let key in user3){
     }
 
 }
+
+// let numbers = [1,2,3];
+
+// // console.log(Object.getPrototypeOf(numbers));
+// console.log(Array.prototype);
+// console.log(numbers);
+
+// function hello(){
+//     console.log("hello");
+// }
+/
