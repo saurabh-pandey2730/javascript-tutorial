02ffed4f0b446df634afd0bd4ba765697bf4fc08
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-30 10:34:23 +0530

    referencing another object

--- javascript.js
+++ javascript.js
@@ -1577,3 +1577,31 @@ console.log(user1);
 const is18 = user1.is18();
 const about = user1.about();
 console.log(about);
+
+
+// 
+
+
+const userMethods = {
+    about : function(){
+        return `${this.firstName} is ${this.age} years old.`;
+    },
+    is18 : function(){
+        return this.age >= 18;
+    }
+}
+function createUser(firstName, lastName, email, age, address){
+    const user = {};
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    user.about = userMethods.about;
+    user.is18 = userMethods.is18;
+    return user;
+}
+
+
+const user1 = createUser('Saurabh', 'Pandey', 'saurabh@gmail.com', 19, "my address");
+console.log(user1.about());
