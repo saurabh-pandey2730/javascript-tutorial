b282365f52fc9f0c0756efe25b18b97df7409de0
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-30 10:26:49 +0530

    this keyword

--- javascript.js
+++ javascript.js
@@ -1487,3 +1487,21 @@ person3.about();
 //     console.log(this);
 // }
 // myFunc();
+
+function about(hobby, favMusician){
+    console.log(this.firstName, this.age, hobby, favMusician);
+}
+const user1 = {
+    firstName : "Rohit",
+    age: 8,   
+}
+const user2 = {
+    firstName : "mohit",
+    age: 9,
+
+}
+
+// apply
+// about.apply(user1, ["guitar", "bach"]);
+// const func = about.bind(user2, "guitar", "bach");
+// func();
