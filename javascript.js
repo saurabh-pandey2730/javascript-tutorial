76963f801e6a336e3383b5bbe0d2609660f31451
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-12-13 13:06:53 +0530

    class keyword

--- javascript.js
+++ javascript.js
@@ -1796,4 +1796,28 @@ for(let key in user3){
 // function hello(){
 //     console.log("hello");
 // }
-/
+
+
+
+//    class 
+
+class CreateUser{
+    constructor(firstName, lastName, email, age, address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.address = address;
+    }
+    about(){
+        return `${this.firstName} is ${this.age} years old.`;
+    }
+    is18(){
+        return this.age >= 18;
+    }
+    sing(){
+        return "la la la la ";
+    }
+}
+const user1 = new CreateUser('Saurabh', 'pandey', 'saurabh@gmail.com', 18, "my address");
+const user2 = new CreateUser('Shivam', 'pandey', 'saurabh@gmail.com', 15, "my address");
