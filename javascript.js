2d3a00f57cf572b78b401cbf831d43ea92063f61
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-12-13 13:11:10 +0530

    getter and setters

--- javascript.js
+++ javascript.js
@@ -1910,3 +1910,25 @@ class Dog extends Animal{
 // console.log(tommy.eat());
 const animal1 = new Animal('sheru', 2);
 console.log(animal1.eat());
+
+// getter and setters 
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+    set fullName(fullName){
+        const [firstName, lastName] = fullName.split(" ");
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+const person1 = new Person("Saurabh", "Pandey", 5);
+// console.log(person1.fullName());
+// console.log(person1.fullName);
+// person1.fullName = "Saurabh Pandey";
+// console.log(person1);
